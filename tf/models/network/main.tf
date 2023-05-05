a3c2754c126f3d27c8a343713a96b9762b0d8dde
Stefania Avallone <stefania.avallone@telecomitalia.it>
2023-05-05 12:41:57 +0200

    use fake_news_resource_group_test

--- tf/models/network/main.tf
+++ tf/models/network/main.tf
@@ -1,43 +1,43 @@
 data "azurerm_client_config" "current" {}
- 
+
 terraform {
-    backend "local" {
-    }
+  backend "local" {
+  }
 }
 
 # Create Resource Group
-resource "azurerm_resource_group" "fake_news_resource_group" {
+resource "azurerm_resource_group" "fake_news_resource_group_test" {
   name     = var.fake_news_resource_group_name
   location = var.location
 }
 
 resource "azurerm_application_insights" "fake_news_insights" {
   name                = var.fake_news_insights_name
-  location            = azurerm_resource_group.fake_news_resource_group.location
-  resource_group_name = azurerm_resource_group.fake_news_resource_group.name
+  location            = azurerm_resource_group.fake_news_resource_group_test.location
+  resource_group_name = azurerm_resource_group.fake_news_resource_group_test.name
   application_type    = "web"
 }
 
 resource "azurerm_key_vault" "fake_news_key_vault" {
   name                = var.fake_news_key_vault_name
-  location            = azurerm_resource_group.fake_news_resource_group.location
-  resource_group_name = azurerm_resource_group.fake_news_resource_group.name
+  location            = azurerm_resource_group.fake_news_resource_group_test.location
+  resource_group_name = azurerm_resource_group.fake_news_resource_group_test.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "premium"
 }
 
 resource "azurerm_storage_account" "fake_news_storage" {
   name                     = var.fake_news_storage_name
-  location                 = azurerm_resource_group.fake_news_resource_group.location
-  resource_group_name      = azurerm_resource_group.fake_news_resource_group.name
+  location                 = azurerm_resource_group.fake_news_resource_group_test.location
+  resource_group_name      = azurerm_resource_group.fake_news_resource_group_test.name
   account_tier             = "Standard"
   account_replication_type = "GRS"
 }
 
 resource "azurerm_machine_learning_workspace" "fake_news_machine_learning" {
   name                          = var.fake_news_machine_learning_name
-  location                      = azurerm_resource_group.fake_news_resource_group.location
-  resource_group_name           = azurerm_resource_group.fake_news_resource_group.name
+  location                      = azurerm_resource_group.fake_news_resource_group_test.location
+  resource_group_name           = azurerm_resource_group.fake_news_resource_group_test.name
   application_insights_id       = azurerm_application_insights.fake_news_insights.id
   key_vault_id                  = azurerm_key_vault.fake_news_key_vault.id
   storage_account_id            = azurerm_storage_account.fake_news_storage.id
@@ -45,18 +45,18 @@ resource "azurerm_machine_learning_workspace" "fake_news_machine_learning" {
   identity {
     type = "SystemAssigned"
   }
-} 
+}
 
 resource "azurerm_virtual_network" "fake_news_network" {
   name                = var.fake_news_network_name
   address_space       = ["10.1.0.0/16"]
-  location            = azurerm_resource_group.fake_news_resource_group.location
-  resource_group_name = azurerm_resource_group.fake_news_resource_group.name
+  location            = azurerm_resource_group.fake_news_resource_group_test.location
+  resource_group_name = azurerm_resource_group.fake_news_resource_group_test.name
 }
 
 resource "azurerm_subnet" "fake_news_subnet" {
   name                 = var.fake_news_subnet_name
-  resource_group_name  = azurerm_resource_group.fake_news_resource_group.name
+  resource_group_name  = azurerm_resource_group.fake_news_resource_group_test.name
   virtual_network_name = azurerm_virtual_network.fake_news_network.name
   address_prefixes     = ["10.1.0.0/24"]
 }
@@ -65,7 +65,7 @@ resource "azurerm_subnet" "fake_news_subnet" {
 
 resource "azurerm_machine_learning_compute_instance" "fake_news_machine_learning_compute_instance" {
   name                          = var.fake_news_machine_learning_compute_instance_name
-  location                      = azurerm_resource_group.fake_news_resource_group.location
+  location                      = azurerm_resource_group.fake_news_resource_group_test.location
   machine_learning_workspace_id = azurerm_machine_learning_workspace.fake_news_machine_learning.id
   virtual_machine_size          = "STANDARD_DS2_V2"
   authorization_type            = "personal"
